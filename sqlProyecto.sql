8ef940cb45690149e353662bd636b9b7d45d1d12
RAMarenco <00080121@uca.edu.sv>
2022-06-13 15:41:20 -0600

    Agregando Tabla Usuario :heavy_plus_sign:

--- sqlProyecto.sql
+++ sqlProyecto.sql
@@ -1,3 +1,22 @@
 CREATE DATABASE BINAES_DB;
 GO
 USE BINAES_DB;
+
+CREATE TABLE USUARIO(
+    id_Usuario VARCHAR(8),
+    nombre VARCHAR(50) NOT NULL,
+    email VARCHAR(50) 
+        NOT NULL 
+		UNIQUE
+		CHECK(email LIKE '%@%'),
+    telefono CHAR(12) 
+		NOT NULL 
+		UNIQUE
+		CHECK(telefono LIKE '+503[2|6|7][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+    ocupacion VARCHAR(15) NOT NULL,
+    direccion VARCHAR(100) NOT NULL 
+		DEFAULT 'Dirección no disponible',
+    fotografia VARBINARY(MAX) NOT NULL,
+    institucion VARCHAR(50) NOT NULL,
+    id_RolUsuario INT NOT NULL
+);
