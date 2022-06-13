de7fcd530f1dab6b483d684d9df02777120e2f7c
mrivasc <00015121@uca.edu.sv>
2022-06-13 15:42:28 -0600

    Agregando tabla Prestamo :heavy_plus_sign:

--- sqlProyecto.sql
+++ sqlProyecto.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE BINAES_DB;
 GO
 USE BINAES_DB;
+
+CREATE TABLE PRESTAMO (
+    id_Prestamo INT NOT NULL, 
+    fh_Prestamo DATETIME NOT NULL,
+    fh_Devolucion DATETIME NOT NULL,
+    id_Estado INT NOT NULL,             -- fk
+    id_UsuarioPresta INT NOT NULL,      -- fk
+    id_Ejemplar INT NOT NULL            -- fk
+);
